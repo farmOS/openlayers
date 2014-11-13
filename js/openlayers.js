67737c529c422e9c631c246d3b067bd37e99ab4c
Peter Philipp <peter.philipp@cando-image.com>
2014-11-14 00:13:43 +0100

    Added support for asynchronous initialization of maps.

--- js/openlayers.js
+++ js/openlayers.js
@@ -1,89 +1,21 @@
 (function($) {
   Drupal.behaviors.openlayers = {
-    'attach': function(context, settings) {
+    attach: function(context, settings) {
 
-      $('.openlayers-map').once('openlayers-map', function() {
+      $('.openlayers-map:not(.asynchronous)').once('openlayers-map', function() {
+        var map_id = $(this).attr('id');
+        Drupal.openlayers.processMap(map_id, context);
+      });
+
+       // Create dynamic callback functions for asynchronous maps.
+      $('.openlayers-map.asynchronous').each(function(){
         var map_id = $(this).attr('id');
         if (Drupal.settings.openlayers.maps[map_id] !== undefined) {
-
-          var object = Drupal.settings.openlayers.maps[map_id];
-          $(document).trigger('openlayers.build_start', [{'type': 'objects', 'objects': object, 'context': context}]);
-
-          var count = 1,
-            layers = object.layer || [],
-            styles = object.style || [],
-            controls = object.control || [],
-            interactions = object.interaction || [],
-            sources = object.source || [],
-            components = object.component || [],
-            objects = {sources: {}, controls: {}, interactions: {}, components: {}, styles: {}, layers: {}, maps: {}};
-
-          object.map.options.layers = [];
-          object.map.options.styles = [];
-          object.map.options.controls = [];
-          object.map.options.interactions = [];
-          object.map.options.components = [];
-
-          try {
-            var map = Drupal.openlayers.getObject(context, 'maps', object.map, null, count);
-            objects.maps[map.machine_name] = map;
-
-            count = sources.length;
-            sources.map(function(data) {
-              if (data.options !== undefined && data.options.attributions !== undefined) {
-                data.options.attributions = [new ol.Attribution({
-                  'html': data.options.attributions
-                })];
-              }
-              objects.sources[data.machine_name] = Drupal.openlayers.getObject(context, 'sources', data, map, count--);
-            });
-
-            count = controls.length;
-            controls.map(function(data) {
-              map.addControl(Drupal.openlayers.getObject(context, 'controls', data, map, count--));
-            });
-
-            count = interactions.length;
-            interactions.map(function(data) {
-              objects.interactions[data.machine_name] = Drupal.openlayers.getObject(context, 'interactions', data, map, count--);
-              map.addInteraction(objects.interactions[data.machine_name]);
-            });
-
-            count = styles.length;
-            styles.map(function(data) {
-              objects.styles[data.machine_name] = Drupal.openlayers.getObject(context, 'styles', data, map, count--);
-            });
-
-            count = layers.length;
-            layers.map(function(data) {
-              data.options.source = objects.sources[data.options.source];
-              if ((data.options.style !== undefined) && (objects.styles[data.options.style] !== undefined)) {
-                data.options.style = objects.styles[data.options.style];
-              }
-              objects.layers[data.machine_name] = Drupal.openlayers.getObject(context, 'layers', data, map, count--);
-              map.addLayer(objects.layers[data.machine_name]);
-            });
-
-            count = components.length;
-            components.map(function(data) {
-              objects.components[data.machine_name] = Drupal.openlayers.getObject(context, 'components', data, map, count--);
-            });
-
-            // Attach data to map DOM object
-            $(document).trigger('openlayers.build_stop', [{'type': 'objects', 'objects': object, 'context': context}]);
-            jQuery('body').data('openlayers', {'objects': objects});
-
-          } catch (e) {
-            if (typeof console !== 'undefined') {
-              Drupal.openlayers.console.log(e.message);
-              Drupal.openlayers.console.log(e.stack);
-            } else {
-              $(this).text('Error during map rendering: ' + e.message);
-              $(this).text('Stack: ' + e.stack);
-            }
+          Drupal.openlayers.asyncIsReadyCallbacks[map_id.replace(/[^0-9a-z]/gi, '_')] = function() {
+            Drupal.openlayers.asyncIsReady(map_id);
           }
         }
-      });
+      })
     }
   };
 
@@ -92,7 +24,103 @@
    */
   Drupal.openlayers = {
 
-    'getObject': (function (context, type, data, map, count) {
+    processMap: function(map_id, context) {
+      if (Drupal.settings.openlayers.maps[map_id] !== undefined) {
+        var object = Drupal.settings.openlayers.maps[map_id];
+        $(document).trigger('openlayers.build_start', [{'type': 'objects', 'objects': object, 'context': context}]);
+
+        var count = 1,
+          layers = object.layer || [],
+          styles = object.style || [],
+          controls = object.control || [],
+          interactions = object.interaction || [],
+          sources = object.source || [],
+          components = object.component || [],
+          objects = {sources: {}, controls: {}, interactions: {}, components: {}, styles: {}, layers: {}, maps: {}};
+
+        object.map.options.layers = [];
+        object.map.options.styles = [];
+        object.map.options.controls = [];
+        object.map.options.interactions = [];
+        object.map.options.components = [];
+
+        try {
+          var map = Drupal.openlayers.getObject(context, 'maps', object.map, null, count);
+          objects.maps[map.machine_name] = map;
+
+          count = sources.length;
+          sources.map(function(data) {
+            if (data.options !== undefined && data.options.attributions !== undefined) {
+              data.options.attributions = [new ol.Attribution({
+                'html': data.options.attributions
+              })];
+            }
+            objects.sources[data.machine_name] = Drupal.openlayers.getObject(context, 'sources', data, map, count--);
+          });
+
+          count = controls.length;
+          controls.map(function(data) {
+            map.addControl(Drupal.openlayers.getObject(context, 'controls', data, map, count--));
+          });
+
+          count = interactions.length;
+          interactions.map(function(data) {
+            objects.interactions[data.machine_name] = Drupal.openlayers.getObject(context, 'interactions', data, map, count--);
+            map.addInteraction(objects.interactions[data.machine_name]);
+          });
+
+          count = styles.length;
+          styles.map(function(data) {
+            objects.styles[data.machine_name] = Drupal.openlayers.getObject(context, 'styles', data, map, count--);
+          });
+
+          count = layers.length;
+          layers.map(function(data) {
+            data.options.source = objects.sources[data.options.source];
+            if ((data.options.style !== undefined) && (objects.styles[data.options.style] !== undefined)) {
+              data.options.style = objects.styles[data.options.style];
+            }
+            objects.layers[data.machine_name] = Drupal.openlayers.getObject(context, 'layers', data, map, count--);
+            map.addLayer(objects.layers[data.machine_name]);
+          });
+
+          count = components.length;
+          components.map(function(data) {
+            objects.components[data.machine_name] = Drupal.openlayers.getObject(context, 'components', data, map, count--);
+          });
+
+          // Attach data to map DOM object
+          $(document).trigger('openlayers.build_stop', [{'type': 'objects', 'objects': object, 'context': context}]);
+          jQuery('body').data('openlayers', {'objects': objects});
+
+        } catch (e) {
+          if (typeof console !== 'undefined') {
+            Drupal.openlayers.console.log(e.message);
+            Drupal.openlayers.console.log(e.stack);
+          } else {
+            $(this).text('Error during map rendering: ' + e.message);
+            $(this).text('Stack: ' + e.stack);
+          }
+        }
+      }
+    },
+
+    // Holds dynamic created asyncIsReady callbacks for every map id.
+    // The functions are named by the cleaned map id. Everything besides 0-9a-z
+    // is replaced by an underscore (_).
+    asyncIsReadyCallbacks: {},
+    asyncIsReady: function (map_id) {
+      if (Drupal.settings.openlayers.maps[map_id] !== undefined) {
+        Drupal.settings.openlayers.maps[map_id].map.async--;
+        if (!Drupal.settings.openlayers.maps[map_id].map.async) {
+          $('#' + map_id).once('openlayers-map', function() {
+            Drupal.openlayers.processMap(map_id, document);
+          });
+        }
+      }
+    },
+
+    getObject: (function (context, type, data, map, count) {
       var cache = $('body').data('openlayers') || {};
 
       if (typeof cache.objects !== 'undefined') {
@@ -132,7 +160,7 @@
      * Falls back to doing nothing in case no such API is available. Simulates
      * the presece of Firebug's console API in Drupal.openlayers.console.
      */
-    'console': (function(){
+    console: (function(){
       var api = {};
       var logger;
       if (typeof(console)==="object" && typeof(console.log)==="function"){
