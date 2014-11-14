ca70ad6dd7ecceb89ff2200f0dcea13e38991cc1
Peter Philipp <peter.philipp@cando-image.com>
2014-11-14 13:39:40 +0100

    Don't fail hard when a vector feature doesn't provide a wkt value.

--- plugins/openlayers/source/openlayers__source__vector.js
+++ plugins/openlayers/source/openlayers__source__vector.js
@@ -10,15 +10,17 @@ Drupal.openlayers.openlayers__source__vector = function(data) {
     }
     var format = new ol.format.WKT();
     for (var i in data.options.features) {
-      var data_projection = data.options.features[i].projection || 'EPSG:4326';
-      var feature = format.readFeature(data.options.features[i].wkt, {
-        dataProjection: data_projection,
-        featureProjection: data.map.getView().getProjection()
-      });
-      if (goog.isDef(data.options.features[i].attributes)) {
-        feature.setProperties(data.options.features[i].attributes);
+      if (data.options.features[i].wkt) {
+        var data_projection = data.options.features[i].projection || 'EPSG:4326';
+        var feature = format.readFeature(data.options.features[i].wkt, {
+          dataProjection: data_projection,
+          featureProjection: data.map.getView().getProjection()
+        });
+        if (goog.isDef(data.options.features[i].attributes)) {
+          feature.setProperties(data.options.features[i].attributes);
+        }
+        options.features.push(feature);
       }
-      options.features.push(feature);
     }
   }
   return new ol.source.Vector(options);
