f2e5e7399c76c3273237555908945403834f0049
Pol Dell'Aiera <Pol@47194.no-reply.drupal.org>
2014-11-17 21:29:04 +0100

    Rename default OL objects.

--- src/Config.php
+++ src/Config.php
@@ -12,7 +12,7 @@ namespace Drupal\openlayers;
 class Config {
   const JS_GROUP = 20;
   const JS_WEIGHT = 10;
-  const EDIT_VIEW_MAP = 'map_view_edit_form';
+  const EDIT_VIEW_MAP = 'openlayers_map_view_edit_form';
   const WATCHDOG_TYPE = 'openlayers';
   // Set this to null to load the 'non-debug' library.
   const LIBRARY_VARIANT = 'debug';
