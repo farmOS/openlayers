f8197ad8dc96058b2494a5d876cf01b609ed62d5
Peter Philipp <peter.philipp@cando-image.com>
2014-11-17 10:48:15 +0100

    More robust code when creating features on the map. Don't take down the whole processing just because of one feature.

--- src/Plugin/Source/vector/js/vector.js
+++ src/Plugin/Source/vector/js/vector.js
@@ -10,15 +10,21 @@ Drupal.openlayers.source__vector = function(data) {
     }
     var format = new ol.format.WKT();
     for (var i in data.options.features) {
-      var data_projection = data.options.features[i].projection || 'EPSG:4326';
-      var feature = format.readFeature(data.options.features[i].wkt, {
-        dataProjection: data_projection,
-        featureProjection: data.map.getView().getProjection()
-      });
-      if (goog.isDef(data.options.features[i].attributes)) {
-        feature.setProperties(data.options.features[i].attributes);
+      if (data.options.features[i].wkt) {
+        try {
+          var data_projection = data.options.features[i].projection || 'EPSG:4326';
+          var feature = format.readFeature(data.options.features[i].wkt, {
+            dataProjection: data_projection,
+            featureProjection: data.map.getView().getProjection()
+          });
+          if (goog.isDef(data.options.features[i].attributes)) {
+            feature.setProperties(data.options.features[i].attributes);
+          }
+          options.features.push(feature);
+        }
+        catch(e) {
+        }
       }
-      options.features.push(feature);
     }
   }
   return new ol.source.Vector(options);
