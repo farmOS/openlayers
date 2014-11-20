b4aa03756d64de83c2a0bcef4c121d13a5f287e5
Pol Dell'Aiera <Pol@47194.no-reply.drupal.org>
2014-11-20 13:15:07 +0100

    Remove attached method from interface.

--- src/Types/LayerInterface.php
+++ src/Types/LayerInterface.php
@@ -18,15 +18,4 @@ interface LayerInterface {
    *   The source assigned to this layer.
    */
   public function getSource();
-
-  /**
-   * Returns a list of attachments for building the render array.
-   *
-   * @param \Drupal\openlayers\Types\ObjectInterface $context
-   *   The object the attachments are attached to.
-   *
-   * @return array
-   *   The attachments to add.
-   */
-  public function attached(\Drupal\openlayers\Types\ObjectInterface $context);
 }
