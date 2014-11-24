4d1fb944b0180c11dffe091552e234e07112a067
Pol Dell'Aiera <Pol@47194.no-reply.drupal.org>
2014-11-24 16:49:39 +0100

    Added an option to the map edit form when the module is enabled. When this checkbox is checked, the map is available through the service module.

--- modules/openlayers_services/themes/openlayers-map--iframe.tpl.php
+++ modules/openlayers_services/themes/openlayers-map--iframe.tpl.php
@@ -43,7 +43,6 @@
 ?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML+RDFa 1.0//EN"
         "http://www.w3.org/MarkUp/DTD/xhtml-rdfa-1.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" version="XHTML+RDFa 1.0">
-
 <head>
   <?php print $styles; ?>
   <?php print $scripts; ?>
