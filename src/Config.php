65d4db8604fcdd48c21c579c64844613ca1a34e0
Pol Dell'Aiera <Pol@47194.no-reply.drupal.org>
2014-12-01 08:06:34 +0100

    Tag JS and CSS files with group = 'openlayers'.

--- src/Config.php
+++ src/Config.php
@@ -10,7 +10,7 @@ namespace Drupal\openlayers;
  * Class openlayers_config.
  */
 class Config {
-  const JS_GROUP = 20;
+  const JS_GROUP = 'openlayers';
   const JS_WEIGHT = 10;
   const EDIT_VIEW_MAP = 'openlayers_map_view_edit_form';
   const WATCHDOG_TYPE = 'openlayers';
