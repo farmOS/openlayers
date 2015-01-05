927c09d9953ea26e0d4a9e52d60178312dd6f581
Pol Dellaiera <Pol@47194.no-reply.drupal.org>
2015-01-05 11:36:56 +0100

    Provide more sources and layers examples, taken from: http://leaflet-extras.github.io/leaflet-providers/preview/

--- src/Openlayers/Source/OSM/js/osm.js
+++ src/Openlayers/Source/OSM/js/osm.js
@@ -1,6 +1,7 @@
 Drupal.openlayers.pluginManager.register({
   fs: 'openlayers.source.internal.osm',
   init: function(data) {
+    data.opt.crossOrigin = null;
     return new ol.source.OSM(data.opt);
   }
 });
