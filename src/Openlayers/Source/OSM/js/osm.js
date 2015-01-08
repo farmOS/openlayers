209dfddef109715bbb097afa50cad5e049c73537
Peter Philipp <peter.philipp@cando-image.com>
2015-01-08 13:17:55 +0100

    More defensive code to ensure missing opt property doesn't cause a hard fail.

--- src/Openlayers/Source/OSM/js/osm.js
+++ src/Openlayers/Source/OSM/js/osm.js
@@ -1,6 +1,9 @@
 Drupal.openlayers.pluginManager.register({
   fs: 'openlayers.source.internal.osm',
   init: function(data) {
+    if (!goog.isDef(data.opt)) {
+      data.opt = {};
+    }
     data.opt.crossOrigin = null;
     return new ol.source.OSM(data.opt);
   }
