445c127f61b41737ac5316ea9d0462ea10b5fa05
Peter Philipp <peter.philipp@cando-image.com>
2015-01-08 15:53:04 +0100

    Added component to configure manual processing of a map.

---
+++ src/Openlayers/Component/LazyProcessing/js/lazyprocessing.js
@@ -0,0 +1,9 @@
+Drupal.openlayers.pluginManager.register({
+  fs: 'openlayers.component.internal.lazyprocessing',
+  init: function(data) {
+  }
+});
+
+Drupal.openlayers.LazyProcessing = function(map_id) {
+
+};
