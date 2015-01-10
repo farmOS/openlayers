2874fbcf8bc69ea718682063a39cac9b6241d77b
Peter Philipp <peter.philipp@cando-image.com>
2015-01-10 15:41:20 +0100

    Remove debugging code.

--- modules/openlayers_geofield/src/Openlayers/Layer/Geofield/js/geofield.js
+++ modules/openlayers_geofield/src/Openlayers/Layer/Geofield/js/geofield.js
@@ -1,7 +1,6 @@
 Drupal.openlayers.pluginManager.register({
   fs: 'openlayers.layer.internal.geofield',
   init: function(data) {
-    console.log(data.opt);
     return new ol.layer.Vector(data.opt);
   }
 });
