fe1b8538c10b59580f517c9540a342217ec9a93e
Pol Dellaiera <Pol@47194.no-reply.drupal.org>
2015-03-05 21:09:21 +0100

    Small update.

--- git-changes.sh
+++ git-changes.sh
@@ -1,5 +1,5 @@
 BASEDIR=$(dirname $0)
 echo "Changes since $2";
 echo "<ul>";
-git log $2..$3 --pretty=format:"<li> <a href='http://drupalcode.org/project/$1.git/commit/%H'>view commit &bull;</a> %s</li> " --reverse | grep -v Merge
+git log $2..$3 --date=short --pretty=format:"<li>%ad: %s (<a href='http://drupalcode.org/project/$1.git/commit/%H'>view commit</a>)</li>" | grep -v Merge
 echo "</ul>";
