175057ef40cd167f0ea19d52bcd4dd3c4740f07d
Peter Philipp <peter.philipp@cando-image.com>
2015-05-31 17:12:08 +0200

    Added possibility to show a layer only on certain zoom levels.

--- src/Openlayers/Layer/Vector/Vector.php
+++ src/Openlayers/Layer/Vector/Vector.php
@@ -16,4 +16,38 @@ $plugin = array(
  */
 class Vector extends Layer {
 
+  /**
+   * {@inheritdoc}
+   */
+  public function defaultProperties() {
+    $default_properties = parent::defaultProperties();
+    $default_properties['options']['zoomActivity'] = '';
+    return $default_properties;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function optionsForm(&$form, &$form_state) {
+    $form['options']['zoomActivity'] = array(
+      '#title' => t('Show on certain zoom levels only'),
+      '#description' => t('Define a zoom level per line, keep empty to show the layer always.'),
+      '#type' => 'textarea',
+      '#default_value' => $this->getOption('zoomActivity'),
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getJS() {
+    $js = parent::getJS();
+    // Ensure we've sane zoom levels.
+    if (!empty($js['opt']['zoomActivity'])) {
+      $js['opt']['zoomActivity'] = array_map('intval', explode("\n", $js['opt']['zoomActivity']));
+      // Ensure the zoom levels are also used as keys.
+      $js['opt']['zoomActivity'] = array_combine($js['opt']['zoomActivity'], $js['opt']['zoomActivity']);
+    }
+    return $js;
+  }
 }
