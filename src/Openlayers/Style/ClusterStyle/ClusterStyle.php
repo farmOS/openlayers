4dda72dca9c7949c8838d8f25b3b075495c6b75a
Pol Dellaiera <Pol@47194.no-reply.drupal.org>
2015-06-01 09:29:00 +0200

    Provide a default style for maps using cluster.
    See: http://openlayers.org/en/v3.5.0/examples/cluster.html

---
+++ src/Openlayers/Style/ClusterStyle/ClusterStyle.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * @file
+ * Style: ClusterStyle.
+ */
+
+namespace Drupal\openlayers\Style;
+use Drupal\openlayers\Types\Style;
+
+$plugin = array(
+  'class' => '\\Drupal\\openlayers\\Style\\ClusterStyle',
+);
+
+/**
+ * Class ClusterStyle.
+ */
+class ClusterStyle extends Style {
+  //TODO: Provide options to let user customize the cluster style.
+}
