4402b6c651c7de7a357b9a219b109730c1da863b
Pol Dellaiera <Pol@47194.no-reply.drupal.org>
2015-07-06 19:17:40 +0200

    Update Icon sprites example and Image static.

---
+++ modules/openlayers_examples/src/Plugin/Component/IconSprites/IconSprites.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * @file
+ * Component: IconSprites.
+ */
+
+namespace Drupal\openlayers_examples\Plugin\Component\IconSprites;
+use Drupal\Component\Annotation\Plugin;
+use Drupal\openlayers\Openlayers;
+use Drupal\openlayers\Types\Component;
+use Drupal\openlayers\Types\ObjectInterface;
+
+/**
+ * Class IconSprites.
+ *
+ * @Plugin(
+ *   id = "IconSprites"
+ * )
+ *
+ */
+class IconSprites extends Component {
+  /**
+   * {@inheritdoc}
+   */
+  public function getJS() {
+    $js = parent::geTJS();
+    $js['opt']['url'] = file_create_url(drupal_get_path('module', 'openlayers_examples') . '/assets/Butterfly.png', array('absolute' => TRUE));
+
+    return $js;
+  }
+}
