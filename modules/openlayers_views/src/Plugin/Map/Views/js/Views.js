03c4f18119b0150aa4454f98284bc5cb4a490763
Pol Dellaiera <Pol@47194.no-reply.drupal.org>
2015-07-08 19:15:06 +0200

    Fix Openlayers Views javascript.

--- modules/openlayers_views/src/Plugin/Map/Views/js/Views.js
+++ modules/openlayers_views/src/Plugin/Map/Views/js/Views.js
@@ -1,30 +1,46 @@
 Drupal.openlayers.pluginManager.register({
   fs: 'openlayers.Map:Views',
-  init: function (data) {
-    var projection = ol.proj.get('EPSG:3857');
-
+  init: function(data) {
     var options = jQuery.extend(true, {}, data.opt);
+    var projection = ol.proj.get('EPSG:3857');
+    var coord = ol.proj.transform([options.view.center.lat, options.view.center.lon], 'EPSG:4326', projection);
 
-    options.view = new ol.View({
-      center: [options.view.center.lat, options.view.center.lon],
+    var view_opts = {
+      center: coord,
       rotation: options.view.rotation * (Math.PI / 180),
       zoom: options.view.zoom,
-      // Todo: Find why these following options makes problems
-      //minZoom: options.view.minZoom,
-      //maxZoom: options.view.maxZoom,
-      projection: projection,
-      extent: projection.getExtent()
-    });
+      projection: projection
+    };
+
+    if (options.view.limit_extent) {
+      if (options.view.limit_extent == 'custom') {
+        // Check if a extent boundaries are set.
+        if (options.view.extent) {
+          view_opts.extent = ol.proj.transform(options.view.extent.replace(/\s*/ig, '').split(','), 'EPSG:4326', projection);
+        }
+      }
+      if (options.view.limit_extent == 'projection') {
+        view_opts.extent = projection.getExtent();
+      }
+    }
+
+    // Just use min / max zoom if set to a non 0 value to avoid problems.
+    if (options.view.minZoom) {
+      view_opts.minZoom = options.view.minZoom;
+    }
+    if (options.view.maxZoom) {
+      view_opts.maxZoom = options.view.maxZoom;
+    }
+
+    options.view = new ol.View(view_opts);
 
     // Provide empty defaults to suppress Openlayers defaults that contains
     // all interactions and controls available.
     options.interactions = [];
     options.controls = [];
+    options.target = data.opt.target;
 
-    var map = new ol.Map(options);
-    map.target = data.opt.target;
-
-    return map;
+    return new ol.Map(options);
   },
   attach: function(context, settings) {},
   detach: function(context, settings) {}
