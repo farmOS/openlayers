cd810265b62ec2466d33b0d5a4f1fd77cda5340b
Pol Dellaiera <Pol@47194.no-reply.drupal.org>
2015-08-02 17:35:39 +0200

    Add the DragFeature interaction.

---
+++ modules/openlayers_library/src/Plugin/Interaction/DragFeature/DragFeature.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * @file
+ * Interaction: DragFeature.
+ */
+
+namespace Drupal\openlayers_library\Plugin\Interaction\DragFeature;
+use Drupal\openlayers\Component\Annotation\OpenlayersPlugin;
+use Drupal\openlayers\Openlayers;
+use Drupal\openlayers\Types\Interaction;
+
+/**
+ * Class DragFeature.
+ *
+ * @OpenlayersPlugin(
+ *  id = "DragFeature",
+ *  description = "You can drag features around on the map."
+ * )
+ */
+class DragFeature extends Interaction {
+
+}
---
+++ modules/openlayers_library/src/Plugin/Interaction/DragFeature/js/DragFeature.js
@@ -0,0 +1,6 @@
+Drupal.openlayers.pluginManager.register({
+  fs: 'openlayers.Interaction:DragFeature',
+  init: function(data) {
+    return new ol.interaction.DragFeature(data.opt);
+  }
+});
