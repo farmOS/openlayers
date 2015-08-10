04f9cc6dd60a129927cbabfc5228c81ecb1354ca
Pol Dellaiera <Pol@47194.no-reply.drupal.org>
2015-08-10 18:43:23 +0200

    crossOrigin parameter fix for XYZ and OSM source plugin.

--- src/Plugin/Source/XYZ/js/xyz.js
+++ src/Plugin/Source/XYZ/js/xyz.js
@@ -1,6 +1,11 @@
 Drupal.openlayers.pluginManager.register({
   fs: 'openlayers.Source:XYZ',
   init: function(data) {
+    if (typeof data.opt.crossOrigin !== 'undefined') {
+      if (data.opt.crossOrigin === 'null') {
+        data.opt.crossOrigin = null;
+      }
+    }
     return new ol.source.XYZ(data.opt);
   }
 });
