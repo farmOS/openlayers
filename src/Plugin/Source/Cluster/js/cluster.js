aa14105a56c1c422b0be4031484b90e48a4e44f7
Peter Philipp <peter.philipp@cando-image.com>
2015-08-18 16:05:15 +0200

    Added options to configure clustering distance per zoom level. Allows to team-up with server side clustering and switching to client side clustering with spiderfier on closer range.

--- src/Plugin/Source/Cluster/js/cluster.js
+++ src/Plugin/Source/Cluster/js/cluster.js
@@ -4,7 +4,32 @@ Drupal.openlayers.pluginManager.register({
     if (typeof data.objects.sources[data.opt.source] !== 'undefined') {
       var options = jQuery.extend(true, {}, data.opt);
       options.source = data.objects.sources[data.opt.source];
-      return new ol.source.Cluster(options);
+
+      // Check if zoom based distance handling is configured.
+      if (typeof data.opt.zoomDistance != 'undefined') {
+        var zoomLevelDistanceHandler = function() {
+          var distance = options.distance;
+          if (typeof data.opt.zoomDistance[data.map.getView().getZoom()] != 'undefined') {
+            distance = data.opt.zoomDistance[data.map.getView().getZoom()];
+          }
+          if (clusterSource.set('distance') != distance) {
+            console.log(distance);
+            clusterSource.set('distance', distance);
+            clusterSource.changed();
+          }
+        };
+        // Change intial distance if it overwrites the default.
+        if (typeof data.opt.zoomDistance[data.map.getView().getZoom()] != 'undefined') {
+          options.distance = data.opt.zoomDistance[data.map.getView().getZoom()];
+        }
+      }
+
+      var clusterSource = new ol.source.Cluster(options);
+      // Attach event handler to source if zoom distances are enabled.
+      if (typeof zoomLevelDistanceHandler != 'undefined') {
+        data.map.getView().on('change:resolution', zoomLevelDistanceHandler);
+      }
+      return clusterSource;
     }
   }
 });
