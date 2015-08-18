cf2181aed21fe11153bd72f353dccfb1d3e93a9e
Peter Philipp <peter.philipp@cando-image.com>
2015-08-18 19:54:19 +0200

    Enhanced the layer zoomActivity handling within the LayerSwitcher.

--- src/Plugin/Layer/Vector/Vector.php
+++ src/Plugin/Layer/Vector/Vector.php
@@ -21,11 +21,18 @@ class Vector extends Layer {
    * {@inheritdoc}
    */
   public function optionsForm(&$form, &$form_state) {
+    $zoom_activity = $this->getOption('zoomActivity', '');
+    // Ensure the values are sorted.
+    if (!empty($zoom_activity)) {
+      $zoom_activity = array_map('intval', explode("\n", trim($this->getOption('zoomActivity', ''))));
+      sort($zoom_activity);
+      implode(PHP_EOL, $zoom_activity);
+    }
     $form['options']['zoomActivity'] = array(
       '#title' => t('Show on certain zoom levels only'),
       '#description' => t('Define a zoom level per line, keep empty to show the layer always.'),
       '#type' => 'textarea',
-      '#default_value' => $this->getOption('zoomActivity', ''),
+      '#default_value' => $zoom_activity,
     );
   }
 
@@ -37,6 +44,8 @@ class Vector extends Layer {
     // Ensure we've sane zoom levels.
     if (!empty($js['opt']['zoomActivity'])) {
       $js['opt']['zoomActivity'] = array_map('intval', explode("\n", $js['opt']['zoomActivity']));
+      // Ensure the values are sorted.
+      sort($js['opt']['zoomActivity']);
       // Ensure the zoom levels are also used as keys.
       $js['opt']['zoomActivity'] = array_combine($js['opt']['zoomActivity'], $js['opt']['zoomActivity']);
     }
