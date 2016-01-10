c257eb150a42a43d961817ea97ec8d082960692d
Pol Dellaiera <Pol@47194.no-reply.drupal.org>
2016-01-10 14:33:15 +0100

    Remove logic related to the Cesium lib from the Component and put it in the main module file.

--- modules/openlayers_cesium/src/Plugin/Component/OL3Cesium/OL3Cesium.php
+++ modules/openlayers_cesium/src/Plugin/Component/OL3Cesium/OL3Cesium.php
@@ -24,15 +24,10 @@ class OL3Cesium extends Component {
    */
   public function attached() {
     $attached = parent::attached();
+
     $attached['libraries_load'][] = array(
       'ol3-cesium',
     );
-
-    $library = libraries_detect('cesium');
-    if ($library['installed'] == TRUE) {
-      $data = "var CESIUM_BASE_URL = '" . url($library['library path'] . '/Build/Cesium/') . "';";
-      $attached['js'][] = file_create_url(file_unmanaged_save_data($data, 'public://openlayers_cesium_base_url.js', FILE_EXISTS_REPLACE), array('absolute' => TRUE));
-    }
 
     return $attached;
   }
